d9b03f27f1c7c6dc6ed615802e9bdea57cd715c9
Yahia Elhag <yahiae6643@hstat.org>
2019-04-07 21:36:27 +0000

    fix navbar error

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 $(function() {
+
+    $('.nav-item').click(function() {
+        $("#navbarSupportedContent").collapse('hide');
+    });
+
+    var scroll = $(window).scrollTop();
+    if (scroll > $(window).height()) {
+        $(".navbar").css("background", "rgba(255,255,255,.75)");
+    }
+
     $(document).scroll(function() {
+
+
         var scroll = $(window).scrollTop();
-        if (scroll > 500) {
+        if (scroll > $(window).height()) {
             $(".navbar").css("background", "rgba(255,255,255,.75)");
         }
         else {
