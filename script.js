b3199ac8b96de02da686880933ce7f8063a025d3
Yahia Elhag <yahiae6643@hstat.org>
2019-04-09 11:02:42 +0000

    fix errors

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(function() {
 
     var scroll = $(window).scrollTop();
     if (scroll > $(window).height()) {
-        $(".navbar").css("background", "rgba(255,255,255,.75)");
+        $(".navbar").css("background", "rgba(255,255,255,.95)");
     }
 
     $(document).scroll(function() {
@@ -14,7 +14,7 @@ $(function() {
 
         var scroll = $(window).scrollTop();
         if (scroll > $(window).height()) {
-            $(".navbar").css("background", "rgba(255,255,255,.75)");
+            $(".navbar").css("background", "rgba(255,255,255,.95)");
         }
         else {
             $(".navbar").css("background", "rgba(255,255,255,0)");
