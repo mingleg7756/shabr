de7ed60effcdc2e9dd04112b4de4ffc6145279f6
Ming Le Gao <mingleg7756@hstat.org>
2019-04-09 14:37:53 +0000

    update index css and javascript

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 $(function() {
-<<<<<<< HEAD
-    $(document).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll > 500) {
-=======
 
     $('.nav-item').click(function() {
         $("#navbarSupportedContent").collapse('hide');
@@ -19,12 +14,7 @@ $(function() {
 
         var scroll = $(window).scrollTop();
         if (scroll > $(window).height()) {
-<<<<<<< HEAD
->>>>>>> 49e64500bb80e214f1dcfadd031241f443ab3798
-            $(".navbar").css("background", "rgba(255,255,255,.75)");
-=======
             $(".navbar").css("background", "rgba(255,255,255,.95)");
->>>>>>> b3199ac8b96de02da686880933ce7f8063a025d3
         }
         else {
             $(".navbar").css("background", "rgba(255,255,255,0)");
